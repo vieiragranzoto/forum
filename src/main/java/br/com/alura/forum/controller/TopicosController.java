47af7297c706eacab5711816e8073e5350758faa
vieiragranzoto <vieiragranzoto@gmail.com>
2023-05-10 19:00:58 -0300

    Add TopicosController

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @RequestMapping("/topicos")
+    @ResponseBody
+    public List<TopicoDto> lista(){
+        List<Topico> topicos= topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
+    }
+
+}
