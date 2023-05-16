c23420605700f7378ea6d3da392f577790324352
vieiragranzoto <vieiragranzoto@gmail.com>
2023-05-16 20:41:19 -0300

    Add cadastro de topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,55 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
+import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+    
+    @Autowired
+    private CursoRepository cursoRepository;
 
-    @RequestMapping("/topicos")
+    
     @ResponseBody
-    public List<TopicoDto> lista(){
-        List<Topico> topicos= topicoRepository.findAll();
+    @GetMapping
+    public List<TopicoDto> lista(String nomeCurso){
+        if(nomeCurso == null){
+            List<Topico> topicos= topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }
+        List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
         return TopicoDto.converter(topicos);
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@Valid @RequestBody TopicoForm form, UriComponentsBuilder uriComponentsBuilder){
+        Curso curso = cursoRepository.findByNome(form.getNomeCurso());
+        Topico topico = form.converter(curso);
+        topicoRepository.save(topico);
+        URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
     }
 
 }
