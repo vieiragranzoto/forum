36b464c131b9141c6c7066f1a233403fc6ef019f
vieiragranzoto <vieiragranzoto@gmail.com>
2023-05-20 14:05:05 -0300

    Add Paginacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,10 +1,13 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -33,23 +37,25 @@ public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
-    
+
     @Autowired
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public List<TopicoDto> listar(String nomeCurso){
+    public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,@PageableDefault(sort = "titulo", direction = Direction.ASC, page = 0, size = 10) Pageable pageable){
+
         if(nomeCurso == null){
-            List<Topico> topicos= topicoRepository.findAll();
+            Page<Topico> topicos= topicoRepository.findAll(pageable);
             return TopicoDto.converter(topicos);
         }
-        List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+        Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, pageable);
         return TopicoDto.converter(topicos);
     }
 
     @PostMapping
     @Transactional
-    public ResponseEntity<TopicoDto> cadastrar(@Valid @RequestBody TopicoForm form, UriComponentsBuilder uriComponentsBuilder){
+    public ResponseEntity<TopicoDto> cadastrar(@Valid @RequestBody TopicoForm form,
+            UriComponentsBuilder uriComponentsBuilder) {
         Curso curso = cursoRepository.findByNome(form.getNomeCurso());
         Topico topico = form.converter(curso);
         topicoRepository.save(topico);
@@ -58,9 +64,9 @@ public class TopicosController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id){
+    public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
         Optional<Topico> topico = topicoRepository.findById(id);
-        if(topico.isPresent()){
+        if (topico.isPresent()) {
             return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
         }
         return ResponseEntity.notFound().build();
@@ -68,9 +74,9 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @Valid @RequestBody AtualizacaoTopicoForm form){
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @Valid @RequestBody AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             Topico topico = form.atualizar(id, topicoRepository);
             return ResponseEntity.ok(new TopicoDto(topico));
         }
@@ -79,9 +85,9 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public ResponseEntity<?> deletar(@PathVariable Long id){
+    public ResponseEntity<?> deletar(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             topicoRepository.deleteById(id);
             return ResponseEntity.ok().build();
         }
